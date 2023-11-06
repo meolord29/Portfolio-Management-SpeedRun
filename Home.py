5176bcad2de933c2637e676d3ef2fde2e861ad2e
meolord29 <53997283+meolord29@users.noreply.github.com>
2023-11-06 17:02:22 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -44,6 +44,17 @@ if authentication_status:
 
         # You can call any Streamlit command, including custom components:
         st.bar_chart(np.random.randn(50, 3))
+        
+        
+    with st.container():
+        col1, col2 = st.columns([3, 1])
+        data = np.random.randn(10, 1)
+
+        col1.subheader("A wide column with a chart")
+        col1.line_chart(data)
+
+        col2.subheader("A narrow column with the data")
+        col2.write(data)
     
     
 elif authentication_status is False:
