0407e242f972d7906880ce7ffb077ca25ab9711f
ryan-lh <ryanlh@connect.hku.hk>
2023-11-20 10:30:56 +0800

    Stuff

--- pages/4_Manage.py
+++ pages/4_Manage.py
@@ -16,9 +16,9 @@ if st.session_state.authentication_status:
     for i, num in enumerate(pf_df.loc[st.session_state.username]):
         col1, col2 = st.columns(2)
         col1.write(pf_df.columns[i])
-        temp = col2.number_input('', 0.0, 1.0, float(num), 0.05, key=pf_df.columns[i], label_visibility='collapsed')
+        temp = col2.number_input(pf_df.columns[i], 0.0, 1.0, float(num), 0.05, key=pf_df.columns[i], label_visibility='collapsed')
 
     if st.session_state.username in pf_df.index:
-        st.plotly_chart(px.pie(pf_df.loc[st.session_state.username]), hole=0.4)
+        st.plotly_chart(px.pie(pf_df, values=st.session_state.username, hole=0.4))
     else:
         st.write('not found')
