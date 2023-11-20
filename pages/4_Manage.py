18166b0d319c6f68c13b285f6c422345d4f67a46
ryan-lh <ryanlh@connect.hku.hk>
2023-11-20 10:23:59 +0800

    Stuff

--- pages/4_Manage.py
+++ pages/4_Manage.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
@@ -11,12 +12,13 @@ if 'authentication_status' not in st.session_state:
     
 if st.session_state.authentication_status:
     pf_df = pd.read_csv('database/datasets/portfolio.csv', index_col=0)
+
     for i, num in enumerate(pf_df.loc[st.session_state.username]):
         col1, col2 = st.columns(2)
         col1.write(pf_df.columns[i])
         temp = col2.number_input('', 0.0, 1.0, float(num), 0.05, key=pf_df.columns[i], label_visibility='collapsed')
 
     if st.session_state.username in pf_df.index:
-        st.write(pf_df.loc[st.session_state.username])
+        st.plotly_chart(px.pie(pf_df.loc[st.session_state.username]), hole=0.4)
     else:
         st.write('not found')
