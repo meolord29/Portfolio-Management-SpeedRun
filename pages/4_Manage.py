386c73e2f832d6f0376223fc52128c027083b591
ryan-lh <ryanlh@connect.hku.hk>
2023-11-20 10:59:16 +0800

    Stuff

--- pages/4_Manage.py
+++ pages/4_Manage.py
@@ -5,20 +5,26 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-
 if 'authentication_status' not in st.session_state:
     st.info('Please Login from the Home page and try again.')
     st.stop()
-    
+
 if st.session_state.authentication_status:
     pf_df = pd.read_csv('database/datasets/portfolio.csv', index_col=0)
+    main_col1, main_col2 = st.columns(2)
+    with main_col1:
+        df3 = pf_df.T.loc[(pf_df != 0).any()]
+        st.plotly_chart(px.pie(df3, values=st.session_state.username, names=df3.index, hole=0.4,
+                               title='Your Current Stock Allocation'), use_container_width=True)
 
-    for i, num in enumerate(pf_df.loc[st.session_state.username]):
-        col1, col2 = st.columns(2)
-        col1.write(pf_df.columns[i])
-        temp = col2.number_input(pf_df.columns[i], 0.0, 1.0, float(num), 0.05, key=pf_df.columns[i], label_visibility='collapsed')
+    with main_col2:
+        for i, num in enumerate(pf_df.loc[st.session_state.username]):
+            col1, col2 = st.columns(2)
+            col1.write(pf_df.columns[i])
+            temp = col2.number_input(pf_df.columns[i], 0.0, 1.0, float(num), 0.05, key=pf_df.columns[i],
+                                     label_visibility='collapsed')
 
     if st.session_state.username in pf_df.index:
-        st.plotly_chart(px.pie(pf_df, values=st.session_state.username, hole=0.4))
+        st.plotly_chart(px.pie(pf_df.T.loc[(pf_df.T != 0).any(1)], values=st.session_state.username, hole=0.4))
     else:
         st.write('not found')
