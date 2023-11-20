463e85dc2abef32b57635631d2f784f9bbce3585
ryan-lh <ryanlh@connect.hku.hk>
2023-11-20 10:17:15 +0800

    Stuff

--- pages/4_Manage.py
+++ pages/4_Manage.py
@@ -11,13 +11,10 @@ if 'authentication_status' not in st.session_state:
     
 if st.session_state.authentication_status:
     pf_df = pd.read_csv('database/datasets/portfolio.csv', index_col=0)
-    col1, col2 = st.columns(2)
-    with col1:
-        for col in pf_df.columns:
-            st.write(col)
-    with col2:
-        for i, num in enumerate(pf_df.loc[st.session_state.username]):
-            temp = st.number_input('', 0.0, 1.0, float(num), 0.05, key=pf_df.columns[i], label_visibility='collapsed')
+    for i, num in enumerate(pf_df.loc[st.session_state.username]):
+        col1, col2 = st.columns(2)
+        col1.write(pf_df.columns[i])
+        temp = col2.number_input('', 0.0, 1.0, float(num), 0.05, key=pf_df.columns[i], label_visibility='collapsed')
 
     if st.session_state.username in pf_df.index:
         st.write(pf_df.loc[st.session_state.username])
